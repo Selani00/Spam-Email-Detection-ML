dc289c8d6ff14a09a936553d5549a5ab24e50286
Dinuk Pathiraja <139424859+Pathiraja-D@users.noreply.github.com>
2024-01-16 16:37:00 +0530

    Completed

--- app.py
+++ app.py
@@ -8,16 +8,16 @@ logging.info('Starting the app...')
 
 data = dict()
 
- 
-NBOutput = ''
-LROutput = ''
 
-@app.route('/')
+NBOutput = ""
+LROutput = ""
+
+@app.route("/")
 def index():
     logging.info('.................Open Home Page.................')
     data['NBOutput'] = NBOutput
     data['LROutput'] = LROutput
-    return render_template('index.html', data=data)
+    return render_template('index.html',data=data)
 
 @app.route("/",methods=['post'])
 def my_post():
@@ -25,25 +25,30 @@ def my_post():
     logging.info(f'Text: {text}')
     preprocessed_text = preprocess_text(text)
     logging.info(f'Preprocessed Text: {preprocessed_text}')
-    vectorized_input = vectorizer([preprocessed_text])
+    vectorized_input = vectorizer(preprocessed_text)
     logging.info(f'Vectorized Input: {vectorized_input}')
     predictionNB = get_prediction_from_NB(vectorized_input)
-    if(predictionNB[0]==1):
+    if (predictionNB[0]==1):
         global NBOutput
         NBOutput = 'Not Spam'
     else:
         NBOutput = 'Spam'
-    logging.info(f'NB Prediction: {NB_prediction}')
+    logging.info(f'NB Prediction: {predictionNB}')
     predictionLR = get_prediction_from_LR(vectorized_input)
-    if(predictionLR[0]==1):
+    if (predictionLR[0]==1):
         global LROutput
         LROutput = 'Not Spam'
     else:
+
         LROutput = 'Spam'
-    logging.info(f'LR Prediction: {LR_prediction}')
+    logging.info(f'LR Prediction: {predictionLR}')
     return redirect(request.url)
     
 
 
-if(__name__ == '__main__'):
+if __name__ == '__main__':
     app.run()
+
+
+    
+
